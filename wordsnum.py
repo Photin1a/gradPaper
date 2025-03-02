b878431eae18fcb7c524bd7873027bff63882497
Photin1a <2574195342@qq.com>
2025-03-02 12:35:22 +0800

    update

---
+++ wordsnum.py
@@ -0,0 +1,11 @@
+import re
+
+# 打开文件并读取内容
+with open('C:\\Users\\photinia\\Desktop\\毕业论文\\document.tex', 'r', encoding='utf-8') as file:
+    text = file.read()
+
+# 使用正则表达式匹配中文字符
+chinese_chars = re.findall(r'[\u4e00-\u9fa5]', text)
+
+# 输出中文字符的数量
+print(f"中文字符数量: {len(chinese_chars)-1500}")
